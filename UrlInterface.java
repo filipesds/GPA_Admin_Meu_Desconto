f07acd8d5b1fabbd8290415189eb55efae1b51bf
filipesds <40299787+filipesds@users.noreply.github.com>
2018-07-18 17:39:51 -0300

    exclusão para ajustes e atualização do projeto.

--- UrlInterface.java
+++
@@ -1,7 +0,0 @@
-package utils;
-
-public interface UrlInterface {
-    public static final String MEUDESCONTODEV = "meudescontodev";
-    public static final String MEUDESCONTOHLG = "meudescontohlg";
-
-}
